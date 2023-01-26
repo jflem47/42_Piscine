b6fbad241ae18c280314df73f66866928c19f2c4
Jean-François Lemieux <58086398+jflem47@users.noreply.github.com>
2023-01-26 13:12:34 -0500

    Delete C_08 directory

--- C_08/ex01/ft_boolean.h
+++
@@ -1,8 +0,0 @@
-#define TRUE 1
-#define FALSE 0
-#define EVEN_MSG "I have an even number of arguments.\n"
-#define ODD_MSG "I have an odd number of arguments.\n"
-#define SUCCESS 0
-#define t_bool int
-#define EVEN(int) int % 2 == 0
-#include <unistd.h>
